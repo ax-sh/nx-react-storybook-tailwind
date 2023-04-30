1393343fa0a237ac5bc65e5ffaad4b2e6e3592bb
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-05-01 01:21:56 +0545

    feat: add stories

---
+++ apps/prototype/src/app/components/Button.stories.tsx
@@ -0,0 +1,26 @@
+// Button.stories.ts|tsx
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button, ButtonProps } from './Button';
+
+const meta: Meta<typeof Button> = {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Button',
+  component: Button,
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {
+  args: {
+    primary: true,
+    label: 'Button',
+    m:[1,2,4],
+    t:{g:2}
+  },
+};
---
+++ apps/prototype/src/app/components/Button.tsx
@@ -0,0 +1,3 @@
+export function Button(){
+    return <button className="bg-red-500 text-white p-4" >Button</button>
+}
